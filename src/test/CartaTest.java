21d806db1da13741059d019b61fda52304c85e05
FABIAN OROZCO CHAVES <fabian.orozcochaves@ucr.ac.cr>
2022-07-05 14:05:08 -0600

    Cartas cargan su propia imagen

--- src/test/CartaTest.java
+++ src/test/CartaTest.java
@@ -11,22 +11,22 @@ public class CartaTest {
 
   @Before
   public void setUp() throws Exception {
-    carta1 = new Carta('P', 1, "/img/back.png");
+    carta1 = new Carta('P', 1);
   }
 	
   @Test
   public void constructores() {
-    Carta carta2 = new Carta();
-    Assert.assertEquals(carta2.getNumero(), 0);
-    Assert.assertEquals(carta2.getPalo(), ' ');
+    Carta carta2 = new Carta('C',2);
+    Assert.assertEquals(carta2.getNumero(), 2);
+    Assert.assertEquals(carta2.getPalo(), 'C');
     Assert.assertEquals(carta1.getNumero(), 1);
     Assert.assertEquals(carta1.getPalo(), 'P');
   }
 
   @Test
   public void toStringYCartasEspeciales(){
-    Carta carta2 = new Carta('D', 13, "/img/back.png");
-    Carta carta3 = new Carta('C', 9, "/img/back.png");
+    Carta carta2 = new Carta('D', 13);
+    Carta carta3 = new Carta('C', 9);
     Assert.assertEquals(carta1.toString(), "A de P");
     Assert.assertEquals(carta2.toString(), "K de D");
     Assert.assertEquals(carta3.toString(), "9 de C");
