23d25d236aab035c8c38e9e19afa7c44ea61c93a
DANIEL ESCOBAR GIRALDO <daniel.escobargiraldo@ucr.ac.cr>
2022-07-26 11:30:08 -0600

    se implementa logica con nuevos metodos

--- src/jamesBond/VistaTablero.java
+++ src/jamesBond/VistaTablero.java
@@ -9,7 +9,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
